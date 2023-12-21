736bd5fb284e4a3f219f82fde606e1c30beb09a7
Anthony Volk <anth.volk@gmail.com>
2023-12-20 22:45:53 -0600

    chore: Lint and changelog

--- tests/python/test_liveness.py
+++ tests/python/test_liveness.py
@@ -1,5 +1,6 @@
 from tests.python.utils import client
 
+
 def test_calculate_liveness(client):
     """This tests that, when passed relevant data, calculate endpoint functions properly"""
     response = client.post(
@@ -12,6 +13,7 @@ def test_calculate_liveness(client):
         ),
     )
     assert response.status_code == 200, response.text
+
 
 def test_calculate_full_liveness(client):
     """This tests that, when passed relevant data, calculate endpoint functions properly"""
