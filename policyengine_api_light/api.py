206097ecf1b26c0d183e0815de1c1d7761d58e8d
Anthony Volk <anth.volk@gmail.com>
2023-12-20 19:58:16 -0600

    chore: Remove economy, metadata, and policy folders

--- policyengine_api_light/api.py
+++ policyengine_api_light/api.py
@@ -5,13 +5,9 @@ from flask_cors import CORS
 import flask
 from .constants import VERSION
 
-# from werkzeug.middleware.profiler import ProfilerMiddleware
-
 # Endpoints
-
 from .endpoints import (
     get_home,
-    get_metadata,
     get_calculate,
 )
 
@@ -23,14 +19,11 @@ CORS(app)
 
 app.route("/", methods=["GET"])(get_home)
 
-app.route("/<country_id>/metadata", methods=["GET"])(get_metadata)
-
 app.route("/<country_id>/calculate", methods=["POST"])(get_calculate)
 
 app.route("/<country_id>/calculate-full", methods=["POST"])(
     (lambda *args, **kwargs: get_calculate(*args, **kwargs, add_missing=True))
 )
-
 
 @app.route("/liveness_check", methods=["GET"])
 def liveness_check():
@@ -38,24 +31,10 @@ def liveness_check():
         "OK", status=200, headers={"Content-Type": "text/plain"}
     )
 
-
 @app.route("/readiness_check", methods=["GET"])
 def readiness_check():
     return flask.Response(
         "OK", status=200, headers={"Content-Type": "text/plain"}
     )
-
-
-# Add OpenAPI spec (__file__.parent / openapi_spec.yaml)
-
-# with open(Path(__file__).parent / "openapi_spec.yaml", encoding="utf-8") as f:
-#     openapi_spec = yaml.safe_load(f)
-#     openapi_spec["info"]["version"] = VERSION
-
-
-# @app.route("/specification", methods=["GET"])
-# def get_specification():
-#     return flask.jsonify(openapi_spec)
-
 
 print("API initialised.")
