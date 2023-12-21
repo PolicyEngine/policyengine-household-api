069403d1a0cd0f6f84b4afb46cfa3401e870f6fa
Anthony Volk <anth.volk@gmail.com>
2023-12-20 22:41:47 -0600

    chore: Add tests to ensure sync between API and API light

--- tests/python/utils.py
+++ tests/python/utils.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+import requests
 from policyengine_api_light.api import app
 
 @pytest.fixture
@@ -6,3 +8,15 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+def extract_json_from_file(filepath):
+  extracted_data = None
+  
+  with open(
+    filepath,
+    "r",
+    encoding="utf-8"
+  ) as file:
+    extracted_data = json.load(file)
+  
+  return extracted_data
