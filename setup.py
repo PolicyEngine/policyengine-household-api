13b1b4cdb10c54199f90aaed34ce37b281a1e97c
Anthony Volk <anth.volk@gmail.com>
2024-02-02 22:28:24 +0200

    chore: Lint

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "python-dotenv",
         "pymysql",
         "black==22.12.0",  # This is because policyengine_canada uses black<23
-        "ratelimiter"
+        "ratelimiter",
     ],
     extras_require={
         "dev": [
