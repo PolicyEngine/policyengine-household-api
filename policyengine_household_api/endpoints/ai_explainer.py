021ec3464518b2278ea368fc19cc0bb99c75ba21
Anthony Volk <anth.volk@gmail.com>
2024-12-12 21:12:17 +0100

    feat: Add tracer parsing

--- policyengine_household_api/endpoints/ai_explainer.py
+++ policyengine_household_api/endpoints/ai_explainer.py
@@ -1,6 +1,8 @@
+import json
 from flask import request, Response
 from policyengine_household_api.utils.validate_country import validate_country
 from policyengine_household_api.utils.google_cloud import fetch_from_cloud_bucket
+from policyengine_household_api.utils.tracer import parse_tracer_output
 
 
 @validate_country
@@ -21,9 +23,29 @@ def get_ai_explainer(country_id: str) -> Response:
     variable: str = request.args.get("variable")
 
     # Fetch the tracer output from the Google Cloud bucket
-    tracer_data: dict = fetch_from_cloud_bucket(uuid)
+    try:
+        tracer_data: dict = fetch_from_cloud_bucket(uuid)
+    except Exception as e:
+        return Response(
+            json.dumps(
+                dict(
+                    status="error",
+                    message=f"Error fetching tracer data: {e}",
+                )
+            ),
+            status=500,
+            mimetype="application/json",
+        )
 
     # Parse the tracer for the calculation tree of the variable
+    try:
+        complete_tracer = tracer_data["tracer"]
+        tracer_segment: list[str] = parse_tracer_output(
+            complete_tracer, variable
+        )
+    except Exception as e:
+        print(f"Error parsing tracer output: {str(e)}")
+        raise e
 
     # Generate the AI explainer prompt using the variable calculation tree
 
