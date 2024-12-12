c629f69390fefe1f57aedce2bc701c0706657258
Anthony Volk <anth.volk@gmail.com>
2024-12-12 18:49:03 +0100

    feat: Add validate_country decorator

--- policyengine_household_api/endpoints/household.py
+++ policyengine_household_api/endpoints/household.py
@@ -1,24 +1,21 @@
-from policyengine_household_api.country import (
-    COUNTRIES,
-    validate_country,
-)
 import json
 from flask import Response, request
+from uuid import UUID
 from policyengine_household_api.country import COUNTRIES
+from policyengine_household_api.utils.validate_country import validate_country
 import json
 import logging
 
-
-def get_calculate(country_id: str, add_missing: bool = False) -> dict:
+@validate_country
+def get_calculate(country_id: str, add_missing: bool = False) -> Response:
     """Lightweight endpoint for passing in household JSON objects and calculating without storing data.
 
     Args:
         country_id (str): The country ID.
+        add_missing (bool = False): Whether or not to populate all
+            possible variables into the household object; this is a special
+            use case and should usually be kept at its default setting.
     """
-
-    country_not_found = validate_country(country_id)
-    if country_not_found:
-        return country_not_found
 
     payload = request.json
     household_json = payload.get("household", {})
@@ -27,6 +24,8 @@ def get_calculate(country_id: str, add_missing: bool = False) -> dict:
     country = COUNTRIES.get(country_id)
 
     try:
+        result: dict
+        tracer_uuid: UUID
         result, tracer_uuid = country.calculate(household_json, policy_json)
     except Exception as e:
         logging.exception(e)
@@ -40,9 +39,40 @@ def get_calculate(country_id: str, add_missing: bool = False) -> dict:
             mimetype="application/json",
         )
 
-    return dict(
-        status="ok",
-        message=None,
-        result=result,
-        ai_explainer_uuid=tracer_uuid,
-    )
+    return Response(
+        json.dumps(
+            dict(
+              status="ok",
+              message=None,
+              result=result,
+              ai_explainer_uuid=str(tracer_uuid),
+            )
+        ), 200, mimetype="application/json")
+
+@validate_country
+def get_ai_explainer(country_id: str) -> Response:
+    """
+    Generate an AI explainer output for a given variable in 
+    a particular household.
+
+    Args:
+        country_id (str): The country ID.
+
+    Returns:
+        Response: The AI explainer output or an error.
+    """
+
+    # Validate country
+
+    # Pull the UUID and variable from the query parameters
+
+    # Fetch the tracer output from the Google Cloud bucket
+
+    # Parse the tracer for the calculation tree of the variable
+
+    # Generate the AI explainer prompt using the variable calculation tree
+
+    # Pass all of this to Claude
+
+    # Return Claude's output
+
