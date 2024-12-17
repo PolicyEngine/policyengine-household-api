0fb9c7787ee440a01ce4a7dfda2b54ac29acfa28
Anthony Volk <anth.volk@gmail.com>
2024-12-18 00:43:58 +0100

    fix: Fix settings

--- policyengine_household_api/utils/google_cloud.py
+++ policyengine_household_api/utils/google_cloud.py
@@ -10,12 +10,6 @@ def upload_json_to_cloud_storage(
     Uploads a JSON-formatted string to a Cloud Storage bucket. Modified from Google Cloud documentation:
     https://cloud.google.com/storage/docs/uploading-objects#uploading-an-object
     """
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -40,5 +34,4 @@ def download_json_from_cloud_storage(
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
 
-    blob_raw = blob.download_as_bytes()
-    return blob_raw.decode("utf-8")
+    return blob.download_as_text()
