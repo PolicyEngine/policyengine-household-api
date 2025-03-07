0419e7c07e49a0232b9b8938467ef7ba8a806824
Anthony Volk <anth.volk@gmail.com>
2025-03-07 18:52:49 +0100

    test: Add Impactica test

---
+++ tests/data/customer_households/impactica.py
@@ -0,0 +1,32 @@
+impactica_household = {
+    "households": {
+        "household": {
+            "members": ["you", "Child_0"],
+            "state_code_str": {"2025": "OR"},
+        }
+    },
+    "people": {
+        "Child_0": {"age": {"2025": 1}},
+        "you": {
+            "age": {"2025": 30},
+            "employment_income": {
+                "2025": 4752.0,
+            },
+            "is_medicaid_eligible": {"2025": None},
+        },
+    },
+    "spm_units": {
+        "spm_unit": {
+            "members": ["you", "Child_0"],
+            "snap": {"2025": None},
+            "snap_assets": {"2025": 0.0},
+            "snap_excess_shelter_expense_deduction": {"2025": 672},
+        }
+    },
+    "tax_units": {
+        "tax_unit": {
+            "basic_standard_deduction": {"2025": 0},
+            "members": ["you", "Child_0"],
+        }
+    },
+}
