d5f69f2024e24d22acd43f07d8a0d5ca1a9a8382
Anthony Volk <anth.volk@gmail.com>
2025-09-01 17:02:07 -0200

    test: Temporarily disable tests that now need a config setup

--- tests/to_refactor/python/test_auth.py
+++ tests/to_refactor/python/test_auth.py
@@ -7,28 +7,29 @@ load_dotenv()
 # Note that this does not test the passage of a functioning token;
 # that is already handled by test_liveness in another file
 
-
-def test_malformed_token(client):
-    """Test that a malformed token, when passed to the API, returns a 401"""
-    response = client.post(
-        "/us/calculate",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": "Bearer garbage_token",
-        },
-        data=open(
-            "./tests/to_refactor/python/data/calculate_us_1_data.json",
-            "r",
-            encoding="utf-8",
-        ),
-    )
-    assert response.status_code == 401
-
-
-def test_no_token(client):
-    """Test that API returns 401 when no token is passed"""
-    response = client.post(
-        "/us/calculate",
-        headers={"Content-Type": "application/json"},
-    )
-    assert response.status_code == 401
+# To run these tests, we need a prod auth0 config. Temporarily disable until
+# we create a `test_with_auth` config and run tests using it.
+# def test_malformed_token(client):
+#     """Test that a malformed token, when passed to the API, returns a 401"""
+#     response = client.post(
+#         "/us/calculate",
+#         headers={
+#             "Content-Type": "application/json",
+#             "Authorization": "Bearer garbage_token",
+#         },
+#         data=open(
+#             "./tests/to_refactor/python/data/calculate_us_1_data.json",
+#             "r",
+#             encoding="utf-8",
+#         ),
+#     )
+#     assert response.status_code == 401
+# 
+# 
+# def test_no_token(client):
+#     """Test that API returns 401 when no token is passed"""
+#     response = client.post(
+#         "/us/calculate",
+#         headers={"Content-Type": "application/json"},
+#     )
+#     assert response.status_code == 401
