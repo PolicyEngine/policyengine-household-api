8935c6bd4c8f712873bdd970aa69acbeba5c1a81
Peter Karman <peter@peknet.com>
2025-11-24 16:30:37 -0600

    unneeded

--- docker/start.sh
+++ docker/start.sh
@@ -7,16 +7,6 @@ REDIS_PORT="${REDIS_PORT:-6379}"
 # Start the API
 gunicorn -b :"$PORT" policyengine_household_api.api --timeout 300 --workers 5 --preload &
 
-# Start multiple workers using POSIX-compliant loop
-# TODO worker.py does not seem to exist?
-#i=1
-#while [ $i -le "$WORKER_COUNT" ]
-#do
-#    echo "Starting worker $i..."
-#    python3 policyengine_api/worker.py &
-#    i=$((i + 1))
-#done
-
 # Keep the script running and handle shutdown gracefully
 trap "pkill -P $$; exit 1" INT TERM
 
