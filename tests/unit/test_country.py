ff1f37b2c0b39f77e49c26afa8ccfd1cfc5fb97c
Max Ghenis <mghenis@gmail.com>
2026-04-10 21:31:22 -0400

    Expose policyengine bundle metadata

--- tests/unit/test_country.py
+++ tests/unit/test_country.py
@@ -4,6 +4,7 @@ from tests.fixtures.country import (
     country_id_us,
 )
 from policyengine_household_api.country import PolicyEngineCountry
+from policyengine_household_api.constants import COUNTRY_PACKAGE_VERSIONS
 from uuid import UUID
 
 
@@ -25,7 +26,7 @@ class TestCalculateComputationTree:
         )
 
         # Then a tuple of a valid response and None is returned
-        assert test_uuid_value == None
+        assert test_uuid_value is None
 
     def test_calculate_tree_requested(self):
 
@@ -39,5 +40,21 @@ class TestCalculateComputationTree:
             enable_ai_explainer=True,
         )
 
-        assert type(test_uuid_value) == str
+        assert isinstance(test_uuid_value, str)
         assert UUID(test_uuid_value).version == 4
+
+
+class TestPolicyEngineBundle:
+
+    def test_country_exposes_policyengine_bundle(self):
+        country = PolicyEngineCountry(country_package_name_us, country_id_us)
+
+        assert country.policyengine_bundle == {
+            "model_version": COUNTRY_PACKAGE_VERSIONS[country_id_us],
+            "data_version": None,
+            "dataset": None,
+        }
+        assert (
+            country.metadata["result"]["version"]
+            == COUNTRY_PACKAGE_VERSIONS[country_id_us]
+        )
